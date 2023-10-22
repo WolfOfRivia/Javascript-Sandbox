69bf9406f534d2207a2e2f39fea17fd1101bed53
AJ Harris <shogunaut90@gmail.com>
2023-10-21 19:32:53 -0500

    Replaced my random user generator js code with the course version

--- 09-fetch-and-async-await/02-random-user-project/script.js
+++ 09-fetch-and-async-await/02-random-user-project/script.js
@@ -9,79 +9,65 @@
   - colors for male are steelblue
   - colors for female are rebeccapurple
 */
-// This is my code, it will be replaced with the course code later but it did accomplish the task.
-
-// Get Elements
-const userEl = document.getElementById('user');
-const userBtn = document.getElementById('generate');
-const spinner = document.querySelector('.spinner');
-
-function getUser() {
-
-  // Get data
-  spinner.classList.remove('hidden');
+// This code below is the course code
+function fetchUser() {
+  showSpinner();
   fetch('https://randomuser.me/api')
-    .then((response) => response.json())
+    .then((res) => res.json())
     .then((data) => {
-      console.log(data.results[0]);
+      // console.log(data.results[0]);
+      hideSpinner();
       displayUser(data.results[0]);
-      spinner.classList.add('hidden');
     })
-    .catch((error) => {
-      console.log(error);
-    })
-
-  function displayUser(user) {
-
-    // Show US State if User is from United States
-    let formatLocation;
-    if(user.location.country === 'United States') {
-      formatLocation = user.location.state;
-    } else {
-      formatLocation = user.location.country
-    }
-
-    // Change background color based on gender
-    let bgColor;
-    if(user.gender === 'female') {
-      bgColor = 'rebeccapurple';
-    } else {
-      bgColor = 'steelblue';
-    }
-
-    document.querySelector('body').style.backgroundColor = bgColor;
-
-    userEl.innerHTML = `
-      <div class="flex justify-between">
-        <div class="flex">
-          <img src="${user.picture.large}" alt="" class="w-48 h-48 rounded-full mr-8">
-          <div class="space-y-3">
-            <p class="text-xl">
-              <span class="font-bold">Name:</span>
-              ${user.name.first} ${user.name.last} 
-            </p>
-            <p class="text-xl">
-              <span class="font-bold">Email:</span>
-              ${user.email} 
-            </p>
-            <p class="text-xl">
-              <span class="font-bold">Phone:</span>
-              ${user.phone} 
-            </p>
-            <p class="text-xl">
-              <span class="font-bold">Location:</span>
-              ${user.location.city}, ${formatLocation} 
-            </p>
-            <p class="text-xl">
-              <span class="font-bold">Age:</span>
-              ${user.dob.age} 
-            </p>
-          </div>
-        </div>
-      </div>`;
-  }
-
 }
 
-userBtn.addEventListener('click', getUser);
-window.addEventListener('DOMContentLoaded', getUser);
+function displayUser(user) {
+  const userDisplay = document.querySelector('#user');
+
+  if(user.gender === 'female') {
+    document.body.style.backgroundColor = 'rebeccapurple';
+  } else {
+    document.body.style.backgroundColor = 'steelblue';
+  }
+
+  userDisplay.innerHTML = `
+    <div class="flex justify-between">
+    <div class="flex">
+      <img src="${user.picture.large}" alt="" class="w-48 h-48 rounded-full mr-8">
+      <div class="space-y-3">
+        <p class="text-xl">
+          <span class="font-bold">Name:</span>
+          ${user.name.first} ${user.name.last} 
+        </p>
+        <p class="text-xl">
+          <span class="font-bold">Email:</span>
+          ${user.email}
+        </p>
+        <p class="text-xl">
+          <span class="font-bold">Phone:</span>
+          ${user.phone}
+        </p>
+        <p class="text-xl">
+          <span class="font-bold">Location:</span>
+          ${user.location.city}, ${user.location.country}
+        </p>
+        <p class="text-xl">
+          <span class="font-bold">Age:</span>
+          ${user.dob.age}
+        </p>
+      </div>
+    </div>
+  </div>`;
+}
+
+function showSpinner() {
+  document.querySelector('.spinner').style.display = 'block';
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').style.display = 'none';
+}
+
+document.querySelector('#generate').addEventListener('click', fetchUser);
+
+fetchUser();
