2357525ecce93f198527a0fc7a5a1d2c449b0612
AJ Harris <shogunaut90@gmail.com>
2023-11-01 23:44:53 -0500

    Created animated clock

---
+++ 10-web-and-browser-apis/05-animated-clock/script.js
@@ -0,0 +1,102 @@
+function clock() {  
+  const now = new Date();
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // Setup canvas
+  ctx.save(); // Save the default state
+  ctx.clearRect(0, 0, 500, 500); // Clear rectangle 
+  ctx.translate(250, 250); // Place the clock in the middle of the canvas
+  ctx.rotate(-Math.PI / 2); // Rotate clock -90deg to get the correct time
+
+  // Set default styles
+  ctx.strokeStyle = '#000000';
+  ctx.fillStyle = '#f4f4f4';
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round'; // Lines will have a rounded end
+
+  // Draw clock face/border
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.strokeStyle = '#800000'; // Only applies to the clock because it is in between the save state and restore state (Lines 19 through 21)
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.fill();
+  ctx.restore();
+  
+  // Draw hour lines
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6); // Gives the number of hour lines doubled by whatever number we entered (so 6 will give us 12)
+    ctx.moveTo(135, 0); // Relative to the translate we set on line 9
+    ctx.lineTo(124, 0); // Relative to the translate we set on line 9
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Draw minute lines
+  ctx.save();
+  ctx.lineWidth = 4;
+  for (let i = 0; i < 60; i++) {
+    // This makes it so a minute line doesn't appear where an hour line is located
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(134, 0); // Relative to the translate we set on line 9
+      ctx.lineTo(136, 0); // Relative to the translate we set on line 9
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
+  // Get current time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  // Draw hour hand
+  ctx.save();
+  ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 14;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(80, 0)
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw minute hand
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = '#800000';
+  ctx.lineWidth = 8;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(112, 0)
+  ctx.stroke();
+  ctx.restore();
+
+  // Draw second hand
+  ctx.save();
+  ctx.rotate((sec * Math.PI / 30));
+  ctx.strokeStyle = '#FF7F50';
+  ctx.fillStyle = '#FF7F50';
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(100, 0)
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
+  // console.log(`${hr}:${min}:${sec}`);
+
+  ctx.restore(); // Restore default State
+  requestAnimationFrame(clock);
+}
+
+requestAnimationFrame(clock);
